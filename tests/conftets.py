f708016f6d7bebc8d39d897e7ab866600d6850f3
Layla <76450831+Half-Sun@users.noreply.github.com>
2023-11-27 17:46:36 +0300

    Delete tests/conftets.py

--- tests/conftets.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selene import browser
-
-
-@pytest.fixture(scope= 'function', autouse=True)
-def browser_management():
-    browser.config.base_url = "https://demoqa.com/automation-practice-form"
-    yield
-    browser.quit()
