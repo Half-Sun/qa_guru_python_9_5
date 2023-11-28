a728b7ea5d4202215faaa960dcf2cff62a28a75e
Leila Magomedova <Mydarlingsatan@gmail.com>
2023-11-28 18:44:00 +0300

    Homework finally done

--- tests/test_homework_5.py
+++ tests/test_homework_5.py
@@ -1,14 +1,14 @@
-from selene import browser, have, be
+from selene import browser, have, be, command
 import time
 
 browser.config.timeout = 40
 browser.config.window_width = 1920
 browser.config.window_height = 1080
-browser.config.base_url = "http://app.qa.guru/automation-practice-form/"
+browser.config.base_url = "http://app.qa.guru"
 
 
 def test_fill_form():
-    browser.open('/')
+    browser.open('/automation-practice-form/')
     browser.element('[class="MuiBox-root css-1vpe9z"]').should(be.visible).click()
     browser.element('[id=":r0:"]').should(be.blank).type("Olga")
     browser.element('[id=":r1:"]').should(be.blank).type("Someone")
@@ -18,22 +18,19 @@ def test_fill_form():
     browser.element('[data-testid="CalendarIcon"]').click()
     browser.all('[role="gridcell"]').element_by(
     have.attribute('data-timestamp').value('1698872400000')).click().press_escape()
-    time.sleep(3)
     browser.element('[data-testid=language]').element('..').click()
     browser.all('#menu- [role=option]').by(have.exact_text('English')).first.click()
-    time.sleep(3)
     browser.element('[role="radiogroup"] [value="Male"]').click()
     browser.element('[type="checkbox"][value="Sports"]').click()
-    time.sleep(10)
     browser.element('[data-testid="subjects"]').element('..').click()
     browser.all('#menu- [role=option]').element_by(
     have.attribute('data-value').value('Accounting')).click().press_escape()
     browser.element('[data-testid="stateCity"]').element('..').click()
     browser.all('#menu- [role=option]').element_by(
     have.attribute('data-value').value('Illinois')).click().press_escape()
-    browser.element('[type="submit"]').click()
+    browser.element('[type="submit"]').perform(command.js.scroll_into_view).click()
     browser.element('[class="MuiTypography-root MuiTypography-h4 css-rq8zac"]').should(have.exact_text('Thank you for submitting the form'))
-    browser.element('[class="MuiTypography-root MuiTypography-body1 css-1qye57c"]').should(have.exact_text('Olga'))
+    browser.all('[class="MuiTypography-root MuiTypography-body1 css-1qye57c"]')[0].should(have.exact_text('Olga'))
     browser.all('[class="MuiTypography-root MuiTypography-body1 css-1qye57c"]')[1].should(have.text('Someone'))
     browser.all('[class="MuiTypography-root MuiTypography-body1 css-1qye57c"]')[2].should(have.text('Pochta@gmail.com'))
     browser.all('[class="MuiTypography-root MuiTypography-body1 css-1qye57c"]')[3].should(have.text('Male'))
